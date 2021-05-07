4162499ed437d8d5cca27b4cec891df36e8e2013
51921 <51921@LAPTOP-NNHIOOM7>
2021-05-06 19:19:08 -0500

    Solución_Casos_Grupal

---
+++ src/Solucion_Guia1_PF/Caso10.java
@@ -0,0 +1,24 @@
+package Solucion_Guia1_PF;
+
+import java.util.Scanner;
+
+public class Caso10 {
+	
+	public static final double PI = 3.1416;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Ingrese radio: ");
+		double r = sc.nextDouble();
+		
+		double area = PI * Math.pow(r, 2);
+		double perimetro = 2 * PI * r;
+		
+		System.out.println("Área del círculo..: " + area);
+		System.out.println("Perímetro.........: " + perimetro);
+		
+				
+		
+
+	}
+}
---
+++ src/Solucion_Guia1_PF/Caso7.java
@@ -0,0 +1,19 @@
+package Solucion_Guia1_PF;
+
+import java.text.DecimalFormat;
+
+public class Caso7 {
+
+	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("#.##");
+		
+		float rpta = (float) ((float) (17 * 2 + (float) Math.pow(3,2) -6) / 5 - 2 * (4.7 - 1.2 * 2));
+		
+		
+		System.out.println("----------------------");
+		System.out.println("Resultados");
+		System.out.println("----------------------");
+		System.out.println("Respuesta...........: " + df.format(rpta));
+		
+	}
+}
